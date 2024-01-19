4e08bd97fa69aee5dc8ad97cc3b0f16cce995f99
benloh <ben@inquirium.net>
2024-01-19 11:10:02 -0800

    refine-label-ui: Use placeholder for AutoSuggest #121
    Fixes Target node field does not remove ellipses on selection when editing an edge

--- app/view/netcreate/components/NCAutoSuggest.jsx
+++ app/view/netcreate/components/NCAutoSuggest.jsx
@@ -41,6 +41,7 @@
 
 const React = require('react');
 const UNISYS = require('unisys/client');
+const { EDGE_NOT_SET_LABEL } = require('system/util/constant');
 
 /// CONSTANTS & DECLARATIONS //////////////////////////////////////////////////
 /// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -283,6 +284,7 @@ class NCAutoSuggest extends UNISYS.Component {
           onKeyDown={this.m_UIKeyDown}
           onFocus={this.m_UIInputFocus}
           onClick={this.m_UIInputClick}
+          placeholder={EDGE_NOT_SET_LABEL}
           autoComplete="off" // turn off Chrome's default autocomplete, which conflicts
         />
         <br />
