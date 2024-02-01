484c4b18621fe0462507afd15b2e0c6a61df344c
DSri Seah <david@davidseah.com>
2024-02-01 10:34:25 -0500

    mod-ur: add example of mini app module

---
+++ _ur_addons/midi/midi-init.ts
@@ -0,0 +1,23 @@
+/*///////////////////////////////// ABOUT \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\*\
+
+  mini app demo
+  this is the init file that's built by @midi-build.mts
+
+\*\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\ * /////////////////////////////////////*/
+
+import { ConsoleStyler } from '@ursys/netcreate';
+import { InitMIDI } from './midi-test';
+
+/// CONSTANTS & DECLARATIONS //////////////////////////////////////////////////
+/// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+const PR = ConsoleStyler('MIDI', 'TagPurple');
+
+
+/// RUNTIME METHODS ///////////////////////////////////////////////////////////
+/// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+console.log(...PR('Demo of mini appserver for tiny projects in UR'));
+InitMIDI();
+
+/// EXPORTS ///////////////////////////////////////////////////////////////////
+/// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+export { InitMIDI };
