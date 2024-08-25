43e2b2c70b07e5ce308bcd8660dd746232153c92
benloh <ben@inquirium.net>
2024-08-25 13:48:11 -0700

    resizable-table: Add URCommentVBtn.  A purely visual component that renders based on passed props and callbacks.  A simplification of MEME's URCommentBtnAlias.

---
+++ app/view/netcreate/components/URCommentVBtn.jsx
@@ -0,0 +1,50 @@
+/*//////////////////////////////// ABOUT \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\*\
+
+  URCommentVBtn
+
+  A purely visual button.
+
+  It shows three visual states:
+  * read/unread status
+    * has unread comments (gold color)
+    * all comments are read (gray color)
+  * is open / selected (displaying comments)
+  * the number of comments.
+
+
+
+\*\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\ * //////////////////////////////////////*/
+
+import React, { useState, useEffect } from 'react';
+import CMTMGR from '../comment-mgr';
+
+/// CONSTANTS & DECLARATIONS //////////////////////////////////////////////////
+/// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+/// REACT FUNCTIONAL COMPONENT ////////////////////////////////////////////////
+/// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+function URCommentVBtn({ id, count, hasUnreadComments, selected, cb }) {
+  let icon;
+  if (count === 0) {
+    icon = CMTMGR.ICN_COMMENT_UNREAD;
+  } else if (selected) {
+    if (hasUnreadComments) icon = CMTMGR.ICN_COMMENT_UNREAD_SELECTED;
+    else icon = CMTMGR.ICN_COMMENT_READ_SELECTED;
+  } else {
+    if (hasUnreadComments) icon = CMTMGR.ICN_COMMENT_UNREAD;
+    else icon = CMTMGR.ICN_COMMENT_READ;
+  }
+
+  return (
+    <button id={id} onClick={cb} className="URCommentVBtn">
+      {icon}
+      <div className={`count ${hasUnreadComments ? 'unread' : ''}`}>
+        {count === 0 ? '' : count}
+      </div>
+    </button>
+  );
+}
+
+/// EXPORT REACT COMPONENT ////////////////////////////////////////////////////
+/// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+export default URCommentVBtn;
